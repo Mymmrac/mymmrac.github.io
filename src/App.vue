1639fca7c61e75354c12ac83231ce78ecc89d12d
Artem Yadelskyi <mymmrac@gmail.com>
2022-08-12 11:27:32 +0300

    Added basic info

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,43 @@
 <template>
     <div class="container mx-auto p-4 my-8 rounded shadow bg-gradient-to-bl from-cyan-400 to-blue-400">
-        <div class="bg-white rounded p-2 shadow">
-            <p class="text-4xl">
-                Go!
-                <icon n="nf-dev-go" class="text-4xl"/>
-                Go!
-            </p>
+        <div class="bg-white rounded p-2 shadow prose max-w-none">
+            <section>
+                <h1>Artem Yadelskyi</h1>
+                <h2>Golang Developer / Software Engineer</h2>
+            </section>
+            <div class="m-divider from-amber-200 to-yellow-400"></div>
+            <section>
+                <h2>Contact info</h2>
+                <ul class="flex flex-wrap gap-2 list-inside">
+                    <li>
+                        <icon n="nf-mdi-email"/>
+                        Email: <a href="mailto:mymmrac@gmail.com">mymmrac@gmail.com</a>
+                    </li>
+                    <li>
+                        <icon n="nf-fa-github"/>
+                        GitHub: <a href="https://github.com/mymmrac">@mymmrac</a>
+                    </li>
+                    <li>
+                        <icon n="nf-fae-telegram"/>
+                        Telegram: <a href="https://t.me/mymmrac">@mymmrac</a>
+                    </li>
+                    <li>
+                        <icon n="nf-fa-twitter"/>
+                        Twitter: <a href="https://twitter.com/mymmrac">@mymmrac</a>
+                    </li>
+                    <li>
+                        <icon n="nf-fa-linkedin"/>
+                        LinkedIn: <a href="https://www.linkedin.com/in/mymmrac">@mymmrac</a>
+                    </li>
+                </ul>
+            </section>
+            <div class="m-divider from-amber-200 to-yellow-400"></div>
+            <section>
+                <h2>Summery</h2>
+                <p>
+                    ...
+                </p>
+            </section>
         </div>
     </div>
 </template>
@@ -13,3 +45,9 @@
 <script setup lang="ts">
 import Icon from "@/components/Icon.vue"
 </script>
+
+<style scoped lang="scss">
+.m-divider {
+    @apply rounded shadow p-2 bg-gradient-to-bl;
+}
+</style>
--- src/icons-map.ts
+++ src/icons-map.ts
@@ -1,5 +1,11 @@
 const icons = new Map<string, string>([
-    ["nf-dev-go", "e724"],
+    [ "nf-dev-go", "e724" ],
+    [ "nf-mdi-email", "f6ed" ],
+    [ "nf-fa-github", "f09b" ],
+    [ "nf-fa-telegram", "f2c6" ],
+    [ "nf-fae-telegram", "e217" ],
+    [ "nf-fa-twitter", "f099" ],
+    [ "nf-fa-linkedin", "f0e1" ],
 ])
 
 export default icons
